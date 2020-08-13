30bc86bc240b60815470e381b1d3d0ebf1f5fef1
AnonProductions <46707105+AnonProductions@users.noreply.github.com>
2020-08-12 23:16:50 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ const cheerio = require('cheerio');
 const URL =
   'https://www.downloadpirate.com/tropic-colour-slide-film-free-download/';
 
-const links = [];
+const dlLinks = links => {
+  links.forEach(link => {
+    const server = link.split('/')[2];
+    findLinks(link, server);
+  });
+};
 
 const findLinks = (link, match) => {
+  const links = [];
   request(link, function(error, response, body) {
     if (error) {
       return console.error('There was an error!');
@@ -19,11 +25,12 @@ const findLinks = (link, match) => {
       const link = $(this).attr('href');
 
       if (link && link.includes(match)) {
-        // links.push(link);
+        links.push(link);
         console.log(link);
-        const prefix = link.split('/')[2];
       }
     });
+
+    dlLinks(links);
   });
 };
 
